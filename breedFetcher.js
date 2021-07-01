f28773e92ba7624830419e593ba1730ea960f727
mh.mozaffari <mh.mozaffari1985@gmail.com>
2021-07-01 18:19:13 -0400

    complete

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,24 @@
 const request = require("request");
-let args = process.argv.splice(2);
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`,
-  function(err, res, body) {
-    //console.log(body);
-    console.log(typeof body);
-    const data = JSON.parse(body);
-    console.log(data[0].description);
-    console.log(typeof data);
-  }
-);
+//let breedName  = "Siberian";
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, resp, body) => {
+      // console.log(body);
+      // console.log(typeof body);
+      if (error) {
+        callback(`Failed to request details: ${error}`, null);
+      }
+      const data = JSON.parse(body);
+      // console.log(data);
+      const breed = data[0];
+      if (breed) {
+        callback(null, breed.description);
+      } else {
+        callback(`Failed to find breed ${breedName}`, null);
+      }
+    }
+  );
+};
+
+module.exports = { fetchBreedDescription };
